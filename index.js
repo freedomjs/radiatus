cdb35717c1025e998f053098439f0fb57efc34e8
Will Scott <willscott@gmail.com>
2014-07-08 14:16:14 -0700

    files mapped such that radiatus can serve as a web server for freedom.js apps

--- index.js
+++ index.js
@@ -27,61 +27,10 @@ var fileServer = require('./fileServer').serve(opts.path);
 
 var app = express();
 app.all('/freedom/*', userRouter.route);
+app.get('/freedom.js', function(req, res) {
+  res.end(require('fs').readFileSync('node_modules/freedom/freedom.js'));
+});
 app.get('*', fileServer.route.bind(fileServer));
 
 app.listen(opts.port);
 console.log("Radiatus is running on port " + opts.port);
-
-/*
-var manifest = JSON.parse(require('fs').readFileSync(opts.path)),
-    index;
-
-if (manifest.app && manifest.app.index) {
-  index = manifest.app.index;
-} else {
-  console.error("Could not run", opts.path, ": No Entry point specified.");
-  process.exit(1);
-}
-index = fs.readFileSync(path.resolve(path.dirname(opts.path), index));
-
-var server = http.createServer(function(request, response) {
-  // Index page.
-  if (request.url == '/index.html' || request.url == '/') {
-    response.writeHead(200, {
-      'Content-Length': index.length,
-      'Context-Type': 'text/html'
-    });
-    response.end(index);
-    return;
-  }
-  // Static Content.
-  else if (manifest.app.static &&
-      manifest.app.static.indexOf(request.url.substr(1)) > -1) {
-    return;
-  }
-
-  // freedom.js
-  else if (request.url == '/freedom.js') {
-    fs.readFile('node_modules/freedom/freedom.js', "binary", function(err, file) {
-      response.writeHead(200, {
-        'Content-Length': file.length,
-        'Content-Type': 'text/javascript'
-      });
-      response.write(file, "binary");
-      response.end();
-    });
-    return;
-  }
-
-  // Otherwise...
-  response.writeHead(404);
-  response.end();
-  return;
-
-  if (userRouter.sessionExists(request, response)) {
-    
-  } else {
-    
-  }
-});
-*/
