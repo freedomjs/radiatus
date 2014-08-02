0942630afed150a5ae92a659c0b44d074a21b4fc
Raymond Cheng <ryscheng@cs.washington.edu>
2014-08-01 19:39:35 -0700

    Add signup form works

--- src/auth.js
+++ src/auth.js
@@ -11,54 +11,38 @@ passport.use('local-login', new LocalStrategy({
 }, function(req, username, password, done) {
   User.findOne({ username: username }, function(err, user) {
     if (err) { return done(err); }
-    //if (!user) { return done(null, false, { message: 'Unknown user ' + username }); }
     if (!user) { return done(null, false, req.flash('loginMessage', 'Incorrect username/password')); }
     user.comparePassword(password, function(err, isMatch) {
       if (err) return done(err);
       if(isMatch) {
         return done(null, user);
       } else {
-        //return done(null, false, { message: 'Invalid password' });
         return done(null, false, req.flash('loginMessage', 'Incorrect username/password'));
       }
     });
   });
 }));
 
-/** 
 passport.use('local-signup', new LocalStrategy({
   usernameField: 'username',
   passwordField: 'password',
   passReqToCallback: true
 }, function(req, username, password, done) {
-    console.log("start");
   process.nextTick(function() {
-    console.log("start");
-    User.findOne({ 'local.username': username }, function(err, user) {
-      if (err) {
-        console.error(err);
-        return done(err);
-      } else if (user) {
-        console.error('taken');
-        return done(null, false, req.flash('loginMessage', 'That username is already taken'));
-      }
-
-      console.log(username);
-      console.log(password);
+    User.findOne({ 'username': username }, function(err, user) {
+      if (err) { return done(err); } 
+      if (user) { return done(null, false, req.flash('loginMessage', 'That username is already taken')); }
       var newUser = new User();
-      newUser.local.username = username;
-      newUser.local.password = newUser.generateHash(password);
+      newUser.username = username;
+      newUser.password = password;
       newUser.save(function(err) {
-        if (err) {
-          console.error(err);
-          throw err;
-        }
+        if (err) { console.log('!!!'); throw err;}
         return done(null, newUser);
       });
     }); //User.findOne
   }); //process.nextTick
 })); //passport.Use
-**/
+
 passport.serializeUser(function(user, done) {
   done(null, user.id);
 });
@@ -101,6 +85,10 @@ router.get('/login', function(req, res){
 router.get('/logout', function(req, res) {
   req.logout();
   res.redirect('/');
+});
+
+router.get('/*', function(req, res) {
+  res.redirect('/radiatus/auth/account');
 });
 
 module.exports = router
--- src/user.js
+++ src/user.js
@@ -9,9 +9,9 @@ mongoose.connection.once('open', function callback() {
 });
 
 var userSchema = mongoose.Schema({
-    username: { type: String, required: true, unique: true },
-    password: { type: String, required: true}
-    //email: { type: String, required: true, unique: true },
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true}
+  //email: { type: String, required: true, unique: true },
 });
 
 // Bcrypt middleware - salt passwords
