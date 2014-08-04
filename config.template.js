0164f80606406d9b913a9731480f4a9026b7f0c2
Raymond Cheng <ryscheng@cs.washington.edu>
2014-08-03 21:22:19 -0700

    Thinking about replacing dependencies

--- config.template.js
+++ config.template.js
@@ -3,5 +3,8 @@ module.exports = {
   cookieKey: 'session',
   userDB: 'mongodb://localhost:27017/radiatus',
   openRegistration: true,
-  saltWorkFactor: 10
+  saltWorkFactor: 10,
+  replaceDependency: {
+    social: 'node_modules/radiatus-providers/src/providers/social.radiatus.json'
+  }
 };
