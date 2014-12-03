caff3b87ae1f99f0753b3fdd0efe6e7aa6222967
Raymond Cheng <ryscheng@cs.washington.edu>
2014-12-03 17:05:32 -0500

    removing gulp. adding browserify task

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,44 +14,50 @@
 
 module.exports = function(grunt) {
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+    pkg: grunt.file.readJSON("package.json"),
+    browserify: {
+      client: {
+        files: { "build/freedom.js": [ "src/client/main.js" ] },
+        options: {}
+      }
+    },
     jshint: {
-      grunt: [ 'Gruntfile.js' ],
-      src: [ 'config/**/*', 'src/**/*.js' ],
+      app: [ "*.js", "*.json" ],
+      src: [ "config/**/*", "src/**/*.js" ],
       options: { jshintrc: true }
     },
     jsdoc: {
       dist: {
-        src: [ 'src/**/*.js' ],
-        options: { destination: 'build/doc' },
+        src: [ "src/**/*.js" ],
+        options: { destination: "build/doc" },
       }
     },
     jasmine_node: {
-      all: [ 'src/' ]
+      all: [ "src/" ]
     },
     clean: [],
     prompt: { tagMessage: { options: { questions: [
       {
-        config: 'bump.options.tagMessage',
-        type: 'input',
-        message: 'Enter a git tag message:',
-        default: 'v%VERSION%',
+        config: "bump.options.tagMessage",
+        type: "input",
+        message: "Enter a git tag message:",
+        default: "v%VERSION%",
       }
     ]}}},
     bump: {
       options: {
-        files: ['package.json', 'bower.json'],
+        files: ["package.json", "bower.json"],
         commit: true,
-        commitMessage: 'Release v%VERSION%',
-        commitFiles: ['package.json', 'bower.json'],
+        commitMessage: "Release v%VERSION%",
+        commitFiles: ["package.json", "bower.json"],
         createTag: true,
-        tagName: 'v%VERSION%',
-        tagMessage: 'Version %VERSION%',
+        tagName: "v%VERSION%",
+        tagMessage: "Version %VERSION%",
         push: true,
-        pushTo: 'origin'
+        pushTo: "origin"
       }
     },
-    'npm-publish': {
+    "npm-publish": {
       options: {
         requires: [],
         abortIfDirty: true,
@@ -59,34 +65,36 @@ module.exports = function(grunt) {
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-jasmine-node');
-  grunt.loadNpmTasks('grunt-jsdoc');
-  grunt.loadNpmTasks('grunt-prompt');
-  grunt.loadNpmTasks('grunt-npm');
-  grunt.loadNpmTasks('grunt-bump');
+  grunt.loadNpmTasks("grunt-contrib-clean");
+  grunt.loadNpmTasks("grunt-contrib-jshint");
+  grunt.loadNpmTasks("grunt-browserify");
+  grunt.loadNpmTasks("grunt-jasmine-node");
+  grunt.loadNpmTasks("grunt-jsdoc");
+  grunt.loadNpmTasks("grunt-prompt");
+  grunt.loadNpmTasks("grunt-npm");
+  grunt.loadNpmTasks("grunt-bump");
 
-  grunt.registerTask('release', function(arg) {
+  grunt.registerTask("release", function(arg) {
     if (arguments.length === 0) {
-      arg = 'patch';
+      arg = "patch";
     }
     grunt.task.run([
-      'build',
-      'test',
-      'prompt:tagMessage',
-      'bump:'+arg,
-      'npm-publish'
+      "build",
+      "test",
+      "prompt:tagMessage",
+      "bump:"+arg,
+      "npm-publish"
     ]);
   });
   
   // Default tasks.
-  grunt.registerTask('build', [
-    'jshint',
+  grunt.registerTask("build", [
+    "browserify:client"
   ]);
-  grunt.registerTask('test', [
-    'jasmine_node',
+  grunt.registerTask("test", [
+    "jshint",
+    "jasmine_node",
   ]);
   
-  grunt.registerTask('default', ['build', 'test']);
+  grunt.registerTask("default", ["build", "test"]);
 };
---
+++ src/client/main.js
@@ -0,0 +1,4 @@
+
+var Consumer = require("freedom/src/consumer");
+
+console.log("freedom.js stub loaded");
